32e5aa6ef35a9e6fd63cad0b8e538e90702122ab
Gene Johnson <jedigene@gmail.com>
2015-05-26 18:57:22 -0700

    Update triangles.rb
    Refactored triangles.rb to clarify and simplify processes.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -6,28 +6,46 @@ class Triangle
     @side_c = side_c
   end
 
-  define_method(:triangle_type) do
-    unless
-       @side_a + @side_b > @side_c &&
-       @side_b + @side_c > @side_a &&
-       @side_c + @side_a > @side_b
-       "Not a triangle"
+  define_method(:is_triangle) do
+    if
+      @side_a >= @side_b + @side_c ||
+      @side_b >= @side_c + @side_a ||
+      @side_c >= @side_a + @side_b
+      false
     else
-      if
-        @side_a == @side_b &&
-        @side_b == @side_c
-        "Equilateral"
-      elsif
-        @side_a == @side_b ||
-        @side_b == @side_c ||
-        @side_c == @side_a
-        "Isosceles"
-      else
-        @side_a != @side_b &&
-        @side_b != @side_c &&
-        @side_c != @side_a
-        "Scalene"
-      end
+      true
+    end
+  end
+
+  define_method(:equilateral) do
+    if
+      @side_a == @side_b &&
+      @side_b == @side_c
+      true
+    else
+      false
+    end
+  end
+  
+  define_method(:isoceles) do
+    if  
+      @side_a == @side_b ||
+      @side_b == @side_c ||
+      @side_c == @side_a
+      true
+    else
+      false
+    end
+  end
+  
+  define_method(:scalene) do
+    if
+      @side_a != @side_b &&
+      @side_b != @side_c &&
+      @side_c != @side_a
+      true
+    end
+      false
     end
   end
 end
